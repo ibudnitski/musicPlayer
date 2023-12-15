7ec1e0042f3bdcc29fbd65a9e45dc77ad1f87822
ibudnitski <igor.budnitski@gmail.com>
2023-12-15 21:34:52 +0200

    Commit

--- index.js
+++ index.js
@@ -1,4 +1,54 @@
+// data
+const playlist = {
+    title: "Metal Hits",
+    coverImageUrl: "./assets/playlistPicture.jpg",
+    info: {
+        totalTracksCount: 2,
+        totalTracksDurationInSeconds: 733,
+    },
+    tracks: [
+        {
+            coverImageUrl: "./assets/pic1.png",
+            artistName: "Sabaton",
+            title: "Panzerkampf",
+            fileUrl: "/.assets/music/Sabaton - Panzerkampf.mp3",
+            isHot: false,
+        },
+        {
+            coverImageUrl: "./assets/pic1.png",
+            artistName: "Powerwolf",
+            title: "We drink your blood",
+            fileUrl: "./assets/music/Powerwolf - We Drink Your Blood.mp3",
+            isHot: true,
+        },
+    ],
+};
 
-const headerTitle = document.createElement('h1');
-headerTitle.innerText = 'Playlist';
-document.body.append(headerTitle);
+// render
+renderPlaylist(playlist)
+
+function renderPlaylist(playlistForRendering) {
+
+    renderPlaylistHeader(playlistForRendering);
+    renderPlaylistTracks(playlistForRendering.tracks);
+
+
+}
+
+function renderPlaylistHeader(playlistForRendering) {
+    function renderPlaylistHeaderTitle(playlistForRendering) {
+        const playlistTitle = document.createElement('h2');
+        playlistTitle.append(playlistForRendering.title);
+        document.body.append(playlistTitle);
+    }
+
+    const  coverElement= document.createElement('img');
+    coverElement.src = playlistForRendering.coverImageUrl;
+    document.body.append(coverElement);
+}
+
+function renderPlaylistTracks(playlistForRendering) {
+    const playlistElement = document.createElement('h2');
+    playlistElement.append('playlist will be here');
+    document.body.append(playlistElement);
+}
